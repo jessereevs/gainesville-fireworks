cbd9b3c2ef6de8ccd8db85556ec26f26f1d69b59
Jesse Reeves <jessereevs@gmail.com>
2024-04-04 21:45:59 -0400

    Added Basic Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import Header from '@/app/ui/header';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Testing Home Page</h1>
+    <main>
+      <Header />
     </main>
   );
 }
