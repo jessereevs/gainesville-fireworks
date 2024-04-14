b9c05b0ff2793d2f701c636e82d3e89538d99aa8
Jesse Reeves <jessereevs@gmail.com>
2024-04-14 19:42:20 -0400

    Add basic shopping cart page structure.

---
+++ app/cart/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import Header from "@/app/ui/header";
+import MainCart from "@/app/ui/cart/main-cart";
+import Footer from "@/app/ui/footer";
+
+export default function Cart() {
+	return(
+		<div>
+			<Header />
+			<MainCart />
+			<Footer />
+		</div>
+	);
+}
---
+++ app/ui/cart/main-cart.tsx
@@ -0,0 +1,9 @@
+export default function MainCart() {
+	return(
+		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="mx-auto mt-12 max-w-3xl">
+				<h1 className="font-bold text-3xl pb-2">Shopping Cart</h1>
+			</div>
+		</div>
+	);
+}
