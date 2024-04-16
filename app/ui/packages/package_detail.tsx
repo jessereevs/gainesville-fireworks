786629fb32aebaf473e300f149a208607d4cdf7e
Jesse Reeves <jessereevs@gmail.com>
2024-04-16 10:29:53 -0400

    Updates

--- app/ui/packages/package_detail.tsx
+++ app/ui/packages/package_detail.tsx
@@ -1,13 +1,28 @@
+import React, {useState} from "react";
+
 import { FireworkPackage } from "@/types";
 import Image from "next/image";
+
+import Alert from "@/app/ui/cart/item-added-alert";
 
 interface PackageDetailProps {
 	fireworkPackage: FireworkPackage;
 }
 
 export default function PackageDetail({ fireworkPackage }: PackageDetailProps) {
+	const [isVisable, setIsVisable] = useState(false);
+
+	const toggleVisability = () => {
+		setIsVisable(!isVisable);
+	}
+
 	return(
 		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div className="pt-4">
+				{isVisable && (
+				<Alert />
+				)}
+			</div>
 			<div className="mx-auto max-w-3xl py-16 flex">
 				<div className="mr-10">
 					<h1 className="font-bold text-3xl pb-2">{fireworkPackage.name}</h1>
@@ -20,7 +35,7 @@ export default function PackageDetail({ fireworkPackage }: PackageDetailProps) {
 					<li className="text-xs mt-2 italic">Fireworks listed above that are not available at the time of purchase will be replaced with a firework from the same category of equal or greater value.</li>
 					</ul>
 					<h2 className="py-8">{fireworkPackage.description}</h2>
-					<button type="button" className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Purchase Package</button>
+					<button type="button" onClick={toggleVisability} className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">Purchase Package</button>
 				</div>
 				<Image 
 					src={fireworkPackage.image}
