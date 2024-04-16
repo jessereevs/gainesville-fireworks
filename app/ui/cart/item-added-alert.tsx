ef789216ad6f447af86f87966162d8c6b84e8060
Jesse Reeves <jessereevs@gmail.com>
2024-04-16 10:50:54 -0400

    Add cart page.

--- app/ui/cart/item-added-alert.tsx
+++ app/ui/cart/item-added-alert.tsx
@@ -15,7 +15,7 @@ export default function Alert() {
                 type="button"
                 className="rounded-md bg-green-50 px-2 py-1.5 text-sm font-medium text-green-800 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-green-50"
               >
-                View status
+                View Cart 
               </button>
               <button
                 type="button"
