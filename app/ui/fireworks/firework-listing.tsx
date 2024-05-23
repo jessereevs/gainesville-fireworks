ed9f04977ffac5b3ba6c56616a368867fb7356e6
Jesse Reeves <jessereevs@gmail.com>
2024-05-23 15:43:56 -0400

    Updates

--- app/ui/fireworks/firework-listing.tsx
+++ app/ui/fireworks/firework-listing.tsx
@@ -1,26 +1,60 @@
-import { Fragment, useState } from "react";
+import { AwaitedReactNode, Fragment, SetStateAction, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import Individuals from "@/backendData/individuals.json";
 
 const subCategories = [
+  { name: "All", href: "#" },
   { name: "Reloadable Shells", href: "#" },
-  { name: "500 Gram Repeater", href: "#" },
-  { name: "350 Gram Repeater", href: "#" },
-  { name: "200 Gram Repeater", href: "#" },
+  { name: "500 Gram Repeaters", href: "#" },
+  { name: "350 Gram Repeaters", href: "#" },
+  { name: "200 Gram Repeaters", href: "#" },
   { name: "Rockets", href: "#" },
   { name: "Roman Candles", href: "#" },
   { name: "Firecrackers", href: "#" },
   { name: "Fountains", href: "#" },
-  { name: "Novelty's", href: "#" },
-  { name: "Assortments", href: "#" },
+  { name: "Novelty", href: "#" },
+  { name: "Assortment", href: "#" },
 ];
+
+// Define the type for the category order map
+type CategoryOrderMap = {
+  [key: string]: number;
+};
+
+// Create a mapping from category names to their order index
+const categoryOrderMap: CategoryOrderMap = subCategories.reduce((acc: CategoryOrderMap, category, index) => {
+  acc[category.name] = index;
+  return acc;
+}, {});
+
+
 
 export default function FireworkListing() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+
+  const sortFireworksByCategory = (fireworks: any[]) => {
+    return fireworks.sort((a: { category: string }, b: { category: string }) => {
+      const orderA = categoryOrderMap[a.category] || Infinity;
+      const orderB = categoryOrderMap[b.category] || Infinity;
+      return orderA - orderB;
+    });
+  };
+
+  const sortedIndividuals = sortFireworksByCategory(Individuals.individuals);
+
+  const filteredIndividuals = selectedCategory === "All"
+    ? sortedIndividuals
+    : sortedIndividuals.filter((product: { category: string; }) => product.category === selectedCategory);
+
+  const handleCategoryClick = (category: SetStateAction<string>) => {
+    setSelectedCategory(category);
+  };
 
   return (
-    <div className="bg-white min-h-screen">
+    <div className="min-h-screen">
       <div>
         {/* Mobile filter dialog */}
         <Transition.Root show={mobileFiltersOpen} as={Fragment}>
@@ -58,7 +92,7 @@ export default function FireworkListing() {
                     </h2>
                     <button
                       type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400"
+                      className="ml-4 flex h-10 w-10 items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                       onClick={() => setMobileFiltersOpen(false)}
                     >
                       <span className="sr-only">Close menu</span>
@@ -71,13 +105,20 @@ export default function FireworkListing() {
                     <h3 className="sr-only">Categories</h3>
                     <ul
                       role="list"
-                      className="px-2 py-3 font-medium text-gray-900"
+                      className="font-medium text-gray-900 px-2 py-3"
                     >
                       {subCategories.map((category) => (
                         <li key={category.name}>
-                          <a href={category.href} className="block px-2 py-3">
+                          <button
+                            type="button"
+                            className="block px-2 py-3 w-full text-left"
+                            onClick={() => {
+                              handleCategoryClick(category.name);
+                              setMobileFiltersOpen(false);
+                            }}
+                          >
                             {category.name}
-                          </a>
+                          </button>
                         </li>
                       ))}
                     </ul>
@@ -88,14 +129,16 @@ export default function FireworkListing() {
           </Dialog>
         </Transition.Root>
 
-        <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900">
               Fireworks
             </h1>
           </div>
-
-          <section aria-labelledby="products-heading" className="pb-24 pt-6">
+          <section
+            aria-labelledby="products-heading"
+            className="pt-6 pb-24"
+          >
             <h2 id="products-heading" className="sr-only">
               Products
             </h2>
@@ -106,11 +149,17 @@ export default function FireworkListing() {
                 <h3 className="sr-only">Categories</h3>
                 <ul
                   role="list"
-                  className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900"
+                  className="font-medium text-gray-900 space-y-4 pb-6 border-b border-gray-200"
                 >
                   {subCategories.map((category) => (
                     <li key={category.name}>
-                      <a href={category.href}>{category.name}</a>
+                      <button
+                        type="button"
+                        className="block px-2 py-1 w-full text-left"
+                        onClick={() => handleCategoryClick(category.name)}
+                      >
+                        {category.name}
+                      </button>
                     </li>
                   ))}
                 </ul>
@@ -121,12 +170,11 @@ export default function FireworkListing() {
                 <div className="bg-white">
                   <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                      {Individuals.individuals.map((product) => (
+                      {filteredIndividuals.map((product: { id: Key | null | undefined; image: string | undefined; name: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | Promise<AwaitedReactNode> | null | undefined; href: string | undefined; price: string | number | bigint | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<AwaitedReactNode> | null | undefined; }) => (
                         <div key={product.id} className="group relative">
                           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                             <img
                               src={product.image}
-                              alt={product.name}
                               className="bg-white h-full w-full object-scale-down object-center lg:h-full lg:w-full"
                             />
                           </div>
