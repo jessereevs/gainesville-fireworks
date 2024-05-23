1a2f9e7e27308c970dc11f632bba1b7368efc7f8
Jesse Reeves <jessereevs@gmail.com>
2024-05-23 14:54:40 -0400

    Updates

--- app/ui/free-shipping-banner.tsx
+++ app/ui/free-shipping-banner.tsx
@@ -1,16 +1,6 @@
-import { XMarkIcon } from "@heroicons/react/20/solid";
-import React, { useState } from "react";
-
 export default function FreeShippingBanner() {
-  const [bannerIsOpen, setBannerIsOpen] = useState(true);
-
-  const hideBanner = () => {
-    setBannerIsOpen(false);
-  };
-
   return (
     <div>
-      {bannerIsOpen && (
         <div className="flex items-center gap-x-6 bg-red-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
           <p className="text-sm leading-6 text-white">
             <a href="/about">
@@ -26,17 +16,8 @@ export default function FreeShippingBanner() {
             </a>
           </p>
           <div className="flex flex-1 justify-end">
-            <button
-              type="button"
-              onClick={hideBanner}
-              className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
-            >
-              <span className="sr-only">Dismiss</span>
-              <XMarkIcon className="h-5 w-5 text-white" aria-hidden="true" />
-            </button>
           </div>
         </div>
-      )}
     </div>
   );
 }
