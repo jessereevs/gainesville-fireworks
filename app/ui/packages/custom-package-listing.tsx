ac36ff68827a37655442f4b12290c04d00f2417b
Jesse Reeves <jessereevs@gmail.com>
2024-06-02 17:32:28 -0400

    Update custom pack button.

--- app/ui/packages/custom-package-listing.tsx
+++ app/ui/packages/custom-package-listing.tsx
@@ -1,29 +1,14 @@
-import Image from "next/image"
-
 export default function CustomPackageListing() {
-    return(
-          <div className="flex justify-center">
-              <a className="group">
-                <div className="aspect-h-2 aspect-w-3 w-full overflow-hidden rounded-lg">
-                  <Image
-                    src="/images/package/package-100-total.jpg"
-                    alt="Image of the firework package."
-                    height={600}
-                    width={600}
-                    className="h-full w-full object-cover object-center group-hover:opacity-75"
-                  />
-                </div>
-                <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                  <div className="flex gap-2">
-                    <h3>Custom Package</h3>
-                    <h4 className=" text-red-600 italic">(Click Here for Details)</h4>
-                  </div>
-                  <p>Custom Pricing</p>
-                </div>
-                <p className="mt-1 text-sm italic text-gray-500">
-                    Design a custom package to get the most out of your firework show!
-                </p>
-              </a>
-          </div>
-    )
+  return (
+    <div className="flex justify-center mt-10">
+      <a href="/packages/custom" className="border-red-800 bg-red-600 border-2 rounded-lg p-4 hover:bg-red-700">
+        <div className="flex items-center justify-center text-base font-medium text-white">
+          <h3 className="text-4xl">Create a Custom Package</h3>
+        </div>
+        <p className="mt-1 text-sm italic text-white">
+          Design a custom package to get the most out of your firework show!
+        </p>
+      </a>
+    </div>
+  );
 }
