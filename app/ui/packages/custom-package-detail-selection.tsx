d3f2fb9c471c41914dcf9eb3a8444737551ce827
Jesse Reeves <jessereevs@gmail.com>
2024-06-02 20:31:18 -0400

    Changing gears.

---
+++ app/ui/packages/custom-package-detail-selection.tsx
@@ -0,0 +1,7 @@
+export default function CustomPackDetailSelection() {
+  return(
+    <div>
+      This is the selection area.
+    </div>
+  )
+}
--- app/ui/packages/custom-package-detail.tsx
+++ app/ui/packages/custom-package-detail.tsx
@@ -1,9 +1,11 @@
 import CustomPackDetailHead from "./custom-package-detail-head"
+import CustomPackDetailSelection from "./custom-package-detail-selection"
 
 export default function CustomPackageDetail() {
     return(
         <div className="">
             <CustomPackDetailHead />
+            <CustomPackDetailSelection />
         </div>
     )
 }
