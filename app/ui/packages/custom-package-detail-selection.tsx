49446483fbbd414d5925c8a51614a397607e04a7
Jesse Reeves <jessereevs@gmail.com>
2024-06-07 18:17:26 -0400

    Update Funnel Color

--- app/ui/packages/custom-package-detail-selection.tsx
+++ app/ui/packages/custom-package-detail-selection.tsx
@@ -175,7 +175,7 @@ export default function FireworksForm() {
               onClick={() => setMobileFiltersOpen(true)}
             >
               <span className="sr-only">Filters</span>
-              <FunnelIcon className="h-5 w-5" aria-hidden="true" />
+              <FunnelIcon className="h-5 w-5 text-red-600" aria-hidden="true" />
             </button>
           </div>
         </div>
