d0d4e0874722437e3d21628153d0d577e6d2077d
Jesse Reeves <jessereevs@gmail.com>
2024-06-09 22:49:09 -0400

    Update for Paypal

--- app/api/paypalClient.ts
+++ app/api/paypalClient.ts
@@ -1,10 +1,15 @@
 import checkoutNodeJssdk from '@paypal/checkout-server-sdk';
 
 function environment() {
+    const mode = process.env.PAYPAL_MODE; // Default to 'live' if PAYPAL_MODE is not set
     const clientId = process.env.PAYPAL_CLIENT_ID || '';
     const clientSecret = process.env.PAYPAL_CLIENT_SECRET || '';
 
-    return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+    if (mode === 'sandbox') {
+        return new checkoutNodeJssdk.core.SandboxEnvironment(clientId, clientSecret);
+    } else {
+        return new checkoutNodeJssdk.core.LiveEnvironment(clientId, clientSecret);
+    }
 }
 
 function client() {
